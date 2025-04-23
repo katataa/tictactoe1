c55bdcfdd9ac0b07622f1f41fa2c8e8758f23af5
Katriin Sartakov <katriinsarta@gmail.com>
2025-04-23 15:10:56 +0300

    frontend maybe done?

--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // ⬅️ Add this
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'app.dart';
-import 'features/auth/home/home_screen.dart';
+import 'features//auth/home/home_screen.dart';
 import 'features/auth/presentation/login_screen.dart';
 
 void main() async {
@@ -13,10 +14,11 @@ void main() async {
   final user = auth.currentUser;
   final bool verified = user?.emailVerified ?? false;
 
-  runApp(ProviderScope( // 👈 Wrap in this
-    child: MyApp(
-      startScreen: user != null && verified ? const HomeScreen() : const LoginScreen(),
+  runApp(
+    ProviderScope( // ⬅️ Wrap the app
+      child: MyApp(
+        startScreen: user != null && verified ? const HomeScreen() : const LoginScreen(),
+      ),
     ),
-  ));
+  );
 }
-
