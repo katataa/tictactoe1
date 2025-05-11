9219128e18616764b3842789becd89c91c2df282
Katriin Sartakov <katriinsarta@gmail.com>
2025-05-12 01:42:25 +0300

    edit

--- tictactoe/lib/core/constants.dart
+++ tictactoe/lib/core/constants.dart
@@ -1,3 +1,3 @@
-const String kWebSocketUrl = 'ws://192.168.3.22:8080';
+const String kWebSocketUrl = 'ws://000:8080';
 const Duration kDefaultGameTimer = Duration(minutes: 5);
 const String kDefaultOpponent = 'Bot';
